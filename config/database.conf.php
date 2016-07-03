14558b113a8229f3140a72b0129ab30460a05d7c
doronl456 <doronl@karmisoft.com>
2016-07-03 10:59:47 +0300

    03/07/16 10:59

--- config/database.conf.php
+++ config/database.conf.php
@@ -1,6 +1,6 @@
 <?php
 	
-  define('DB_NAME', 'some db name');
+  define('DB_NAME', 'some db name 2');
   define('DB_USER', 'DB-USER');
   define('DB_PASS', 'DB-PASS');
   define('DB_HOST', 'DB-HOST');
